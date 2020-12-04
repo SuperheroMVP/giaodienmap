0505ad706875639d04541e234e52ada1514f913d
Hung Do <dohung28599@gmail.com>
2020-12-04 07:58:16 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function activateMaps() {
+
+    let zoomMap = $('.maps-container-inner').ZoomArea({
+        zoomLevel:0.1,
+        minZoomLevel: 0.2,
+        maxZoomLevel: 30,
+        parentOverflow: 'auto',
+        exceptionsZoom: ['marker-all'],
+        hideWhileAnimate: ['map-area', 'marker-all'],
+        externalIncrease: '.map-control-zoomin',
+        externalDecrease: '.map-control-zoomout',
+        virtualScrollbars: false,
+        usedAnimateMethod: 'jquery'
+    });
+
+    resizeFilterPanel();
+}
+
+function resizeFilterPanel() {
+    $('.map-filter').removeAttr('style');
+    if (parseInt($('.measurement').css('max-width'), 10) > 768) {
+        $('.map-filter').css('height', $('.maps-container').outerHeight() + 'px');
+    }
+}
+
+
+$(window).on("load", function () {
+    activateMaps();
+});
+
+$(window).on("resize", function () {
+    resizeFilterPanel();
+});
